26604026862998963dea272afbb7f083110964d3
net <net@net-r3.cs.hku.hk>
2017-08-16 02:33:22 -0700

    add instructions for setting up the virtual machien

--- vmdev/provision/init-script.sh
+++ vmdev/provision/init-script.sh
@@ -15,3 +15,10 @@ sudo ./install-dependencies.sh
 git submodule update --init
 ./configure.py --enable-dpdk --compiler=g++-5 --mode=release
 ninja
+
+# After scp this file to the virtual machine, boot
+# into the virtual machine. Perform the following steps
+# 1. Open /etc/default/grub
+# 2. In GRUB_CMDLINE_LINUX_DEFAULT, add "console=ttyS0,115200"
+# 3. execute "update-grub"
+# 4. From now on, use "virsh console vm-name" to log in to the vm.
