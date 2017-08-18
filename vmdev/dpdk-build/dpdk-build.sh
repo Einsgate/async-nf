58a8219bbd870aa3c3b8e4b225a42aff2936fcd9
net <net@net-r1.cs.hku.hk>
2017-08-18 15:39:57 +0800

    update dpdk-build.sh

--- vmdev/dpdk-build/dpdk-build.sh
+++ vmdev/dpdk-build/dpdk-build.sh
@@ -2,6 +2,15 @@
 
 git clone -b local-17.02 https://github.com/duanjp8617/dpdk.git
 
-make -C dpdk RTE_OUTPUT=$PWD/build/ config T=x86_64-native-linuxapp-gcc
+cd ./dpdk
 
-make -C build
+make config T=x86_64-native-linuxapp-gcc
+
+make -j8
+
+export RTE_SDK=$PWD
+export RTE_TARGET=build
+
+cd ./examples
+
+make -j8
