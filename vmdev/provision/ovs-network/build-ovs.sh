2c7da199671705a6c68a72073f6720a51fa74046
net <net@net-r1.cs.hku.hk>
2017-08-19 12:39:13 +0800

    add ovs configuration scripts

---
+++ vmdev/provision/ovs-network/build-ovs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# A good reference: https://medium.com/@john.lin/%E7%B7%A8%E8%AD%AF-open-vswitch-v2-7-0-on-ubuntu-16-04-2-lts-6df0a2f7d648
+
+# Require autoreconf, need apt-get install autoconf
+
+DPDK_BUILD=/home/net/async-nf/vmdev/provision/dpdk/build
+
+git clone -b branch-2.7 https://github.com/openvswitch/ovs.git
+
+sudo apt-get update
+
+sudo apt-get install -y dh-autoreconf libssl-dev libcap-ng-dev openssl python python-pip
+
+sudo pip install six
+
+cd ovs
+
+./boot.sh
+
+./configure --with-dpdk=$DPDK_BUILD --with-linux=/lib/modules/$(uname -r)/build
+
+make -j8
+
+sudo make install
---
+++ vmdev/provision/ovs-network/configure-ovs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Reference:
+# https://wiki.qemu.org/Documentation/vhost-user-ovs-dpdk
+# http://docs.openvswitch.org/en/latest/intro/install/dpdk/
+
+CORE_MASK=0x1f
+PMD_CORE_MASK=0x1e
+MEM=2048
+HUGE_PAGE_DIR=/mnt/huge
+
+sudo killall ovsdb-server ovs-vswitchd
+
+sudo rm -f /usr/local/var/run/openvswitch/vhost-user*
+
+sudo rm -f /usr/local/etc/openvswitch/conf.db
+
+sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db /usr/local/share/openvswitch/vswitch.ovsschema
+
+sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --pidfile --detach --log-file
+
+sudo ovs-vsctl --no-wait init
+
+sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-lcore-mask=$CORE_MASK
+
+sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-socket-mem=$MEM
+
+sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-hugepage-dir=$HUGE_PAGE_DIR
+
+sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=$PMD_CORE_MASK
+
+sudo ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
+
+sudo ovs-vswitchd --pidfile --detach --log-file
