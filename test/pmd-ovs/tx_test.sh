c6fcac7fd5dbdc06b9b06c24a529f3b39de92ec6
net <net@net-r1.cs.hku.hk>
2017-08-24 15:27:30 +0800

    add launching scripts created by Junjie

---
+++ test/pmd-ovs/tx_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect -f
+
+set qnum [lindex $argv 0]
+set cpu_num [lindex $argv 1]
+
+spawn sudo /home/net/async-nf/vmdev/dpdk/app/test-pmd/testpmd 0x1F -n 4 --socket-mem 2048 -- --burst=64 -i --txqflags=0xf00 --rxq=$qnum --txq=$qnum --nb-cores=$cpu_num
+
+expect "testpmd>" 
+send "set fwd txonly\r"
+
+expect "testpmd>" 
+send "start\r"
+
+expect "testpmd>" 
+send "show port stats 0\r"
+
+interact
