546ce3b85a66a9b1b4da1eea11c0a274223852b5
duanjp8617 <duanjp8617@gmail.com>
2017-10-31 14:12:14 +0800

    add mica set up scripts

---
+++ test/set-up-mica/install-miac.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# down load dpdk 16.11
+wget http://fast.dpdk.org/rel/dpdk-16.11.3.tar.xz
+
+tar -xvf ./dpdk-16.11.3.tar.xz
+
+cd ./dpdk-stable-16.11.3
+
+make config T=x86_64-native-linuxapp-gcc
+
+make -j
+
+cd ../
+
+git clone https://github.com/efficient/mica2.git
+
+cd mica2/build
+
+ln -s ../../dpdk-stable-16.11.3 ./dpdk
+
+cmake ..
+
+make -j
+
+ln -s ../src/mica/test/*.json .
+
+# possibly execute sudo killall etcd to restart etcd
